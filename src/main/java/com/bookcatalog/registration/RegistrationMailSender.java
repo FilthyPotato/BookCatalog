7aabfaf50c49770ded5acdafd4ad48a09295e3e2
Helios <daniel.kula101@gmail.com>
2018-02-12 19:49:12 +0100

    Rename

--- src/main/java/com/bookcatalog/registration/RegistrationMailSender.java
+++ src/main/java/com/bookcatalog/registration/RegistrationMailSender.java
@@ -5,23 +5,25 @@ import com.bookcatalog.registration.model.VerificationToken;
 import com.bookcatalog.registration.repositories.VerificationTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
-public class RegistrationService {
+@Service
+public class RegistrationMailSender {
     private VerificationTokenRepository verificationTokenRepository;
     private VerificationTokenMaker verificationTokenMaker;
     private MailService mailService;
     private RegistrationMailGenerator registrationMailGenerator;
 
     @Autowired
-    public RegistrationService(VerificationTokenRepository verificationTokenRepository, VerificationTokenMaker verificationTokenMaker,
-                               MailService mailService, RegistrationMailGenerator registrationMailGenerator) {
+    public RegistrationMailSender(VerificationTokenRepository verificationTokenRepository, VerificationTokenMaker verificationTokenMaker,
+                                  MailService mailService, RegistrationMailGenerator registrationMailGenerator) {
         this.verificationTokenRepository = verificationTokenRepository;
         this.verificationTokenMaker = verificationTokenMaker;
         this.mailService = mailService;
         this.registrationMailGenerator = registrationMailGenerator;
     }
 
-    public void initRegistrationConfirmation(User user, String host, String contextName) {
+    public void sendRegistrationMail(User user, String host, String contextName) {
         VerificationToken verificationToken = verificationTokenMaker.createVerificationToken(user);
         verificationTokenRepository.save(verificationToken);
 
--- src/test/java/com/bookcatalog/registration/RegistrationMailSenderTests.java
+++ src/test/java/com/bookcatalog/registration/RegistrationMailSenderTests.java
@@ -1,9 +1,5 @@
 package com.bookcatalog.registration;
 
-import com.bookcatalog.registration.MailService;
-import com.bookcatalog.registration.RegistrationMailGenerator;
-import com.bookcatalog.registration.RegistrationService;
-import com.bookcatalog.registration.VerificationTokenMaker;
 import com.bookcatalog.registration.model.User;
 import com.bookcatalog.registration.model.VerificationToken;
 import com.bookcatalog.registration.repositories.VerificationTokenRepository;
@@ -17,8 +13,8 @@ import org.springframework.mail.SimpleMailMessage;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-public class RegistrationServiceTests {
-    private RegistrationService registrationService;
+public class RegistrationMailSenderTests {
+    private RegistrationMailSender registrationMailSender;
     @Mock
     private VerificationTokenMaker verificationTokenMaker;
     @Mock
@@ -40,7 +36,7 @@ public class RegistrationServiceTests {
 
     @Before
     public void setUp(){
-        registrationService = new RegistrationService(verificationTokenRepository, verificationTokenMaker, mailService, registrationMailGenerator);
+        registrationMailSender = new RegistrationMailSender(verificationTokenRepository, verificationTokenMaker, mailService, registrationMailGenerator);
     }
 
     @Test
@@ -48,7 +44,7 @@ public class RegistrationServiceTests {
         //1. saves verificationToken to db (no need to see if the data is valid, since verificationTokenMaker is
         //responsible for that and it has been tested)
         when(verificationTokenMaker.createVerificationToken(user)).thenReturn(expectedToken);
-        registrationService.initRegistrationConfirmation(user, null, null);
+        registrationMailSender.sendRegistrationMail(user, null, null);
         verify(verificationTokenRepository, times(1)).save(expectedToken);
     }
 
@@ -58,7 +54,7 @@ public class RegistrationServiceTests {
         when(user.getEmail()).thenReturn(mailDestination);
         when(expectedToken.getToken()).thenReturn(token);
         when(registrationMailGenerator.generateMail(mailDestination, host, contextName, token)).thenReturn(mailMessage);
-        registrationService.initRegistrationConfirmation(user, host, contextName);
+        registrationMailSender.sendRegistrationMail(user, host, contextName);
         verify(mailService, times(1)).send(mailMessage);
     }
 }
