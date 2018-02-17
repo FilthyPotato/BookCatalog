a88ea0330b7556f1063412101433cfff37e55f65
Helios <daniel.kula101@gmail.com>
2018-02-17 21:13:01 +0100

    Add test data

--- src/test/resources/test_data.sql
+++ src/test/resources/test_data.sql
@@ -1,8 +1,7 @@
 INSERT INTO ROLE(ID, NAME) VALUES(1, 'ROLE_USER');
 
 INSERT INTO USER(ID, USERNAME, EMAIL, PASSWORD, ENABLED)
-VALUES  (0, 'system', 'system', '#G#$G#$G#$G#', true),
-        (1, 'test1', 'test1@test.com', 'test1', true),
+VALUES  (1, 'test1', 'test1@test.com', 'test1', true),
         (2, 'test2', 'test2@test.com', 'test2', true);
 
 INSERT INTO USERS_ROLES(USER_ID, ROLE_ID)
@@ -11,37 +10,40 @@ VALUES  (1, 1),
 
 INSERT INTO AUTHOR(ID, NAME)
 VALUES  (1, 'Author1'),
-        (2, 'Author2');
+        (2, 'Author2'),
+        (3, 'Author3');
 
 INSERT INTO GENRE(ID, NAME)
 VALUES  (1, 'Genre1'),
-        (2, 'Genre2');
+        (2, 'Genre2'),
+        (3, 'Genre3');
 
 INSERT INTO USER_PROFILE(ID, EMAIL)
-VALUES  (0, 'system'),
-        (1, 'test1@test.com');
+VALUES  (1, 'test1@test.com'),
+        (2, 'test2@test.com');
 
 INSERT INTO SHELF(ID, NAME, USER_PROFILE_ID)
-VALUES  (0, 'systemShelf', 0),
-        (1, 'Shelf1', 1),
+VALUES  (1, 'Shelf1', 1),
         (2, 'Shelf2', 1);
 
 INSERT INTO BOOK(ID, TITLE, DESCRIPTION, PAGES, PUBLISHED_DATE, PUBLISHER)
 VALUES  (1, 'Title1', 'Description1', 100, '2015-06-12', 'Publisher1'),
-        (2, 'Title2', 'Description2', 200, '2015-06-12',  'Publisher2');
+        (2, 'Title2', 'Description2', 200, '2015-06-12',  'Publisher2'),
+        (3, 'Title3', 'Description3', 300, '2015-06-12',  'Publisher3');
 
 INSERT INTO BOOKS_AUTHORS (BOOK_ID, AUTHOR_ID)
 VALUES  (1, 1),
-        (2, 2);
+        (2, 2),
+        (3, 3);
 
 INSERT INTO BOOKS_GENRES (BOOK_ID, GENRE_ID)
 VALUES  (1, 1),
-        (2, 2);
+        (2, 2),
+        (3, 3);
 
 INSERT INTO SHELVES_BOOKS (SHELF_ID, BOOK_ID)
-VALUES  (0, 1),
-        (0, 2),
+VALUES  (1, 1),
         (1, 2),
-        (1, 2);
-
+        (2, 2),
+        (2, 3);
 
